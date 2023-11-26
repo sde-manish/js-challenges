735cdc3a0c6003ce06dcd2f30a5c12ab9cfe5406
Manish Kumar <manish481181@gmail.com>
2023-11-26 15:33:12 +0530

    findMissingLetter

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,34 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+    if (arr.length === 0) {
+        let str = "";
+        return str;
+    }  else {
+        let expectedCharcode = arr[0].charCodeAt(0);
+        for (let i = 0; i <  arr.length; i++) {
+            if (arr[i].charCodeAt(0) !== expectedCharcode) {
+                var missingLetter = String.fromCharCode(expectedCharcode);
+                return missingLetter;
+                break
+            }
+            expectedCharcode++;
+        }
+    }
+}
+
+
+// Another solution 
+
+// function findMissingLetter(arr) {
+//     const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//     const startIndex = alphabet.indexOf(arr[0]);
+  
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] !== alphabet[startIndex + i]) {
+//         return alphabet[startIndex + i];
+//       }
+//     }
+  
+//     return '';
+//   }
 
 module.exports = findMissingLetter;
