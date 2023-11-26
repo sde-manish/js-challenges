a28acc2607e1dd8e927ce4fc55ee57c7cc994995
Manish Kumar <manish481181@gmail.com>
2023-11-26 20:39:56 +0530

    Update first-non-repeating-run.js

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating-run.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating-run.js
@@ -1,7 +1,7 @@
 const findFirstNonRepeatingCharacter = require('./first-non-repeating');
 
-const result1 = findFirstNonRepeatingCharacter('programming');
-const result2 = findFirstNonRepeatingCharacter('abacddbec');
+const result1 = findFirstNonRepeatingCharacter('aabccdeff');
+const result2 = findFirstNonRepeatingCharacter('aabbcc');
 
 console.log(result1);
 console.log(result2);
