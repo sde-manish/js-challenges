000ac10b3308b15ca6c3190414fd6799a10b0b95
Manish Kumar <manish481181@gmail.com>
2023-11-26 18:50:39 +0530

    findFirstNonRepeatingCharacter

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,24 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+    const uniqueCharacters = new Set(str.split(""));
+
+    if (str.length !== uniqueCharacters.size) {
+        return false
+    }
+     return true
+
+
+//  Another solution
+//      const charSet = new Set();
+
+//   for (let i = 0; i < str.length; i++) {
+//     const char = str[i];
+//     if (charSet.has(char)) {
+//       return false;
+//     }
+//     charSet.add(char);
+//   }
+
+//   return true;
+}
 
 module.exports = areAllCharactersUnique;
--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,17 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+    for (let i = 0; i < str.length; i++) {
+        const matched = [];
+        for (let j = i + 1; j < str.length; j++) {
+            if ( str[i] === str[j] ) {
+             matched.push(str[j])
+            }
+        }
+        if (matched.length === 0) {
+            let firstUnique = str[i]
+            return firstUnique
+        }
+    }
+    return null
+}
 
 module.exports = findFirstNonRepeatingCharacter;
