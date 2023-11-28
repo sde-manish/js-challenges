a710348f3edcd4dbcd11a7216ed7b71404e0abf8
Manish Kumar <manish481181@gmail.com>
2023-11-28 19:45:30 +0530

    diceGameSimulation

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,25 @@
-function diceGameSimulation() {}
+function diceGameSimulation(num) {
+    const resultArr = []
+
+    for (let i = 0; i < num; i++) {
+        const dice1 = Math.floor(Math.random()*6 + 1)
+        const dice2 = Math.floor(Math.random()*6 + 1)
+
+        let sum = dice1 + dice2;
+        let result = ""
+
+        if (sum === 7 || sum === 11) {
+            result = "win"
+        } else if (sum === 2 || sum === 3 || sum === 12) {
+            result = "lose"
+        } else {
+            result = "roll again"
+        }
+
+        resultArr.push({dice1, dice2, sum, result})
+        
+    }
+    return resultArr
+}
 
 module.exports = diceGameSimulation;
