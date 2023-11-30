cbd2bb727872d9f2e1ce7c2be5c62527f4a3ed16
Manish Kumar <manish481181@gmail.com>
2023-11-30 19:53:48 +0530

    validateEmail

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,62 @@
-function validateEmail() {}
+function validateEmail(emailStr) {
+
+/*    emailStrArr = emailStr.split("")
+    if (emailStrArr.includes("@") && emailStrArr.includes(".")) {
+        return true
+    }
+
+    return false
+
+The above solutiom i s not correct. */
+
+
+
+// Second correct solution
+if (emailStr.indexOf('@') === -1 || emailStr.indexOf('.') === -1) {
+    return false;
+  }
+  
+const inputStrArr = emailStr.split("@");
+const firstPart = inputStrArr[0]
+const secondPart = inputStrArr[1]
+
+
+if (firstPart.length < 2 || secondPart.length < 3 ) {
+    return false;
+}
+
+const domainExtensionArr = secondPart.split(".")
+
+
+const domain  = domainExtensionArr[0]
+const extension = domainExtensionArr[1]
+if (domain.length < 2 || extension.length < 3) {
+    return false
+}
+
+return true
+}
+//  Another way to write this solution
+// function validateEmail(email) {
+//     if (email.indexOf('@') === -1) {
+//       return false;
+//     }
+  
+//     const [localPart, domain] = email.split('@');
+  
+//     if (localPart.length === 0 || domain.length < 3) {
+//       return false;
+//     }
+  
+//     const domainExtension = domain.split('.');
+//     if (
+//       domainExtension.length < 2 ||
+//       domainExtension[domainExtension.length - 1].length < 2
+//     ) {
+//       return false;
+//     }
+  
+//     return true;
+//   }
 
 module.exports = validateEmail;
