2945070fd9730070099ab092c7845744f99b5e6c
Manish Kumar <manish481181@gmail.com>
2023-12-01 01:49:38 +0530

    calculateTotalSalesWithTax

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,26 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+   
+    // const priceArr = []
+
+    // products.forEach(product => {
+    //     priceArr.push(product.price*product.quantity)
+    // });
+    
+    // const totalsale = priceArr.reduce((acc, currVal) => acc + currVal, 0)
+
+    // const tax = (totalsale*taxRate)/100
+
+    // return totalsaleWithtax = totalsale + tax
+
+
+// Writing in more optimized way
+// return totalSalewithTax = products.reduce((acc, currEle)=> acc + currEle.price*currEle.quantity, 0) + 
+// ((products.reduce((acc, currEle)=> acc + currEle.price*currEle.quantity, 0))*taxRate)/100;
+
+// OR
+const totalsale = products.reduce((acc, currEle)=> acc + currEle.price*currEle.quantity, 0)
+
+return totalsale + (totalsale*taxRate)/100
+}
 
 module.exports = calculateTotalSalesWithTax;
