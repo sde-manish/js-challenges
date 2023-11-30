6bfbbf7158777df502ad1a1dd82904b0de9e4e1d
Manish Kumar <manish481181@gmail.com>
2023-12-01 00:13:37 +0530

    sumOfEvenSquares

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares-run.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares-run.js
@@ -1,5 +1,5 @@
 const sumOfEvenSquares = require('./sum-of-even-squares');
 
-const result = sumOfEvenSquares([1, 2, 3, 4, 5, 6]);
+const result = sumOfEvenSquares([1, 2, 3, 4, 5]);
 
 console.log(result);
--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,6 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+    return arr.filter((ele)=> ele%2===0).map((ele)=> ele*ele).reduce((acc,currVal)=>
+    acc+currVal, 0)
+}
 
 module.exports = sumOfEvenSquares;
