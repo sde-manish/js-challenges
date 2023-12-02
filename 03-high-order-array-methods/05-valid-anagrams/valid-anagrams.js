62018baa527db3a7abe78a4503f1366dd20eedfa
Manish Kumar <manish481181@gmail.com>
2023-12-02 23:37:03 +0530

    validAnagrams

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,14 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+    const str1SortedStr = str1.split("").sort().join("")
+    const str2SortedStr = str2.split("").sort().join("")
+    console.log(str1SortedStr, str2SortedStr);
+
+    if (str1SortedStr === str2SortedStr) {
+        return true
+    }
+
+    return false
+    
+}
 
 module.exports = validAnagrams;
